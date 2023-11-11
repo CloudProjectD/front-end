e82e4441a6e9e8d03717c4ef967cf3c43a92ca94
okod <okodee@naver.com>
2023-11-11 23:56:53 +0900

    feat: add showToast() and GestureDetector #3

--- lib/view/authentication_view.dart
+++ lib/view/authentication_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AuthenticationView extends StatefulWidget {
@@ -9,61 +10,54 @@ class AuthenticationView extends StatefulWidget {
 }
 
 class _AuthenticationViewState extends State<AuthenticationView> {
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController numController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(20.0, 50.0, 20.0, 20.0),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Image(
-                image: AssetImage('assets/universitySeal.png'),
-                width: 200,
-              ),
-              Text(
-                '인증',
-                style: TextStyle(
-                  color: Colors.black87,
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              Text('경희대학교 학생 이메일로 인증해주세요!',
-                  style: TextStyle(
-                    color: Colors.black54,
-                    fontSize: 15.0,
-                  )),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 80.0),
-                child: Column(
+        body: GestureDetector(
+          onTap: (){
+            FocusScope.of(context).unfocus();
+          },
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(20.0, 50.0, 20.0, 20.0),
+              child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("이메일",
-                        style: TextStyle(
-                            color: Colors.black87,
-                            fontSize: 15.0,
-                            fontWeight: FontWeight.w600)),
-                    SizedBox(
-                      height: 5,
+                    SizedBox(height: 60.0,),
+                    Image.asset('assets/universitySeal.png',
+                      width: 70,
                     ),
+                    Text(
+                      '인증',
+                      style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text('경희대학교 학생 이메일로 인증해주세요!',
+                        style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 15.0,
+                        )),
+                    SizedBox(height: 50.0,),
                     Row(
                       children: [
                         Flexible(
                           child: TextField(
-                            onChanged: (value) {
-                              // signUpController.setAdmissionYear(value);
-                            },
-                            onSubmitted: (value) {
-                              // signUpController.setAdmissionYear(value);
-                            },
+                            controller: emailController,
                             keyboardType: TextInputType.emailAddress,
                             cursorColor: Colors.black87,
                             decoration: InputDecoration(
-                              label: Text('이메일을 입력해주세요'),
+                              label: Text('이메일'),
+                              floatingLabelBehavior: FloatingLabelBehavior.never,
                               contentPadding:
-                                  EdgeInsets.fromLTRB(20, 20, 20, 20),
+                              EdgeInsets.fromLTRB(20, 20, 20, 20),
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(10.0),
                                 borderSide: BorderSide.none,
@@ -75,10 +69,12 @@ class _AuthenticationViewState extends State<AuthenticationView> {
                         SizedBox(
                           width: 20.0,
                         ),
-                        Container(
+                        SizedBox(
                           height: 50.0,
                           child: ElevatedButton(
-                            onPressed: () {},
+                            onPressed: () {
+                              showToast("이메일이 전송되었습니다");
+                            },
                             child: Text('전송', style: TextStyle(fontSize: 20.0)),
                             style: ElevatedButton.styleFrom(
                                 backgroundColor: Colors.grey),
@@ -87,30 +83,18 @@ class _AuthenticationViewState extends State<AuthenticationView> {
                       ],
                     ),
                     SizedBox(height: 20.0),
-                    Text("인증번호",
-                        style: TextStyle(
-                            color: Colors.black87,
-                            fontSize: 15.0,
-                            fontWeight: FontWeight.w600)),
-                    SizedBox(
-                      height: 5,
-                    ),
                     Row(
                       children: [
                         Flexible(
                           child: TextField(
-                            onChanged: (value) {
-                              // signUpController.setAdmissionYear(value);
-                            },
-                            onSubmitted: (value) {
-                              // signUpController.setAdmissionYear(value);
-                            },
-                            keyboardType: TextInputType.emailAddress,
+                            controller: numController,
+                            keyboardType: TextInputType.number,
                             cursorColor: Colors.black87,
                             decoration: InputDecoration(
-                              label: Text('인증번호를 입력해주세요'),
+                              label: Text('인증번호'),
+                              floatingLabelBehavior: FloatingLabelBehavior.never,
                               contentPadding:
-                                  EdgeInsets.fromLTRB(20, 20, 20, 20),
+                              EdgeInsets.fromLTRB(20, 20, 20, 20),
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(10.0),
                                 borderSide: BorderSide.none,
@@ -122,7 +106,7 @@ class _AuthenticationViewState extends State<AuthenticationView> {
                         SizedBox(
                           width: 20.0,
                         ),
-                        Container(
+                        SizedBox(
                           height: 50.0,
                           child: ElevatedButton(
                             onPressed: () {},
@@ -133,22 +117,33 @@ class _AuthenticationViewState extends State<AuthenticationView> {
                         ),
                       ],
                     ),
-                  ],
-                ),
-              ),
-              Container(
-                height: 50.0,
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text('다음', style: TextStyle(fontSize: 20.0)),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.grey,
-                  ),
-                ),
-              )
-            ]),
+                    SizedBox(height: 150.0,),
+                    SizedBox(
+                      height: 50.0,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: (){},
+                        child: Text('다음', style: TextStyle(fontSize: 20.0)),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromRGBO(157, 28, 32, 1),
+                        ),
+                      ),
+                    ),
+                  ]),
+            ),
           ),
-        ));
+        )
+    );
   }
+}
+
+void showToast(String message){
+  Fluttertoast.showToast(
+    msg: message,
+    backgroundColor: Colors.white,
+    textColor: Colors.black87,
+
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM,
+  );
 }
